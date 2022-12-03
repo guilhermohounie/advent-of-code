372c975ff00db58ecd98d165909ec449167d196a
Guilhermo Hounie <hounie.guilhermo@gmail.com>
2022-12-03 19:26:18 -0300

    day 3 refactor

--- 2022/3/solution.rb
+++ 2022/3/solution.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'set'
+
 ITEMS = [*'a'..'z', *'A'..'Z'].freeze
 
 # Parse the input file
@@ -10,6 +12,7 @@ class ParseFile
     @rucksacks = []
     File.open(path).each do |line|
       first, second = line.gsub(/\n/, '').partition(/.{#{line.length / 2}}/)[1, 2]
+      # Divide each rucksack in two compartments
       @rucksacks << {
         first_compartment: first,
         second_compartment: second
@@ -31,38 +34,38 @@ class Solution
 
   private
 
-  def shared_item_between_compartments(rucksack)
-    shared_item = nil
-    rucksack[:first_compartment].each_char do |char|
-      shared_item = char if rucksack[:second_compartment].include?(char)
-    end
-    shared_item
+  # Concat the two compartments of a rucksack
+  def rucksack_combine_compartments(rucksack)
+    rucksack.values.join('')
   end
 
+  # Find the item that is shared between n rucksacks
+  def rucksacks_intersections(*rucksacks)
+    rucksacks.map { |rucksack| Set.new(rucksack.split('')) }.reduce(:intersection).to_a[0]
+  end
+
+  # Get the shared item between the two compartments of a rucksack
+  def shared_item_between_compartments(rucksack)
+    Set.new(rucksack[:first_compartment].split('')).intersection(rucksack[:second_compartment].split('')).to_a[0]
+  end
+
+  # Find the item that is shared between n rucksacks
   def shared_item_between_groups(rucksack_group)
-    shared_item = nil
-    first_compartment = rucksack_group[0][:first_compartment].concat(rucksack_group[0][:second_compartment])
-    second_compartment = rucksack_group[1][:first_compartment].concat(rucksack_group[1][:second_compartment])
-    third_compartment = rucksack_group[2][:first_compartment].concat(rucksack_group[2][:second_compartment])
-    first_compartment.each_char do |char|
-      shared_item = char if second_compartment.include?(char) && third_compartment.include?(char)
-    end
-    shared_item
+    elf1 = rucksack_combine_compartments(rucksack_group[0])
+    elf2 = rucksack_combine_compartments(rucksack_group[1])
+    elf3 = rucksack_combine_compartments(rucksack_group[2])
+    rucksacks_intersections(elf1, elf2, elf3)
   end
 
   def solve(rucksacks)
-    sum = 0
-    rucksacks.each do |rucksack|
-      sum += ITEMS.index(shared_item_between_compartments(rucksack)) + 1
+    rucksacks.reduce(0) do |sum, rucksack|
+      sum + ITEMS.index(shared_item_between_compartments(rucksack)) + 1
     end
-    sum
   end
 
   def solve_badges(rucksacks_groups)
-    sum = 0
-    rucksacks_groups.each do |rucksack_group|
-      sum += ITEMS.index(shared_item_between_groups(rucksack_group)) + 1
+    rucksacks_groups.reduce(0) do |sum, rucksack_group|
+      sum + ITEMS.index(shared_item_between_groups(rucksack_group)) + 1
     end
-    sum
   end
 end
